a0e51fa1f2cbf941e92f8147ed89fb9647c42ce2
aru.www <151684138+albergomez777@users.noreply.github.com>
2023-11-28 19:20:12 -0300

    cometer cambios

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
-let peso = 15;
+let dato = document.getElementById("peso");
+let  boton= document.getElementById("calcular");
+let MAN= document.getElementById("mante")
+const VOL = document.getElementById("volumen")
+boton.addEventListener("click",eventoboton);
+function eventoboton() {
+    let peso= dato.valueAsNumber;
+    let volumen;
+    if(peso <= 30){
+        volumen=holliday(peso);
+        VOL.innerHTML= volumen + "cc/dia";
+    
+        VOL.style.display= "block";
+        MAN.innerHTML=volumen / 24;
+        MAN.style.display="block";
+    }else{
+        volumen=superficiecorporal(peso);
+}
+console.log(volumen);
+}
+function holliday(peso) {
+    if(peso> 20){
+        let aux= (peso-20)*20
+        resultado = 1500+aux;
+    }
+    else if (peso<21&&peso>10){
+        resultado=((peso-10)*50+1000);
+    }
+    else{
+        resultado=peso*100
+    }
+    return resultado  
+}
+function superficiecorporal(peso) {
+    let sc= ((peso*4)+7) / (peso+90)
+    return sc;
+}
+ 
 
-let mivolumen = 1;
-
-console.log(peso+1+mivolumen);
-
-const miconstante = 10;
-let volumen=1600;
 
 
-// number,string
+
+
