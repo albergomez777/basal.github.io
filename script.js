d60fc61a1cdebc7fb41fc7081f6cbc20358ad725
aru.www <151684138+albergomez777@users.noreply.github.com>
2023-11-29 00:49:37 -0300

    cometer cambios

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function eventoboton() {
         VOL.innerHTML= volumen/24 + "cc/hr";
     
         VOL.style.display= "block";
-        MAN.innerHTML=volumen / 24 *1.5 +"cc/m+m/2";
+        MAN.innerHTML= "m+m/2 ="+ volumen / 24 *1.5 +"cc/hr";
         MAN.style.display="block";
     }else{
         volumen=superficiecorporal(peso);
@@ -23,13 +23,13 @@ function holliday(peso) {
         let aux= (peso-20)*20
         resultado = 1500+aux;
     }
-    else if (peso<21&&peso>10){
+    else if (peso<=20&&peso>10){
         resultado=((peso-10)*50+1000);
     }
-    else{
-        resultado=peso*100
+    else if (peso <=10){
+        resultado=peso*100;
     }
-    return resultado  
+    return resultado;  
 }
 function superficiecorporal(peso) {
     let sc= ((peso*4)+7) / (peso+90)
