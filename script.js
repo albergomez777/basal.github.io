b5e7e79255f836b3f4d014b421d6a28fc362e96e
aru.www <151684138+albergomez777@users.noreply.github.com>
2023-11-29 19:09:46 -0300

    cometer cambios

--- script.js
+++ script.js
@@ -15,9 +15,8 @@ function eventoboton() {
         MAN.innerHTML= "m+m/2 ="+ volumen / 24 *1.5 +"cc/hr";
         MAN.style.display="block";
     }else{
-        volumen=superficiecorporal(peso);
+        superficiecorporal(peso);
 }
-console.log(volumen);
 }
 function holliday(peso) {
     if(peso> 20){
@@ -33,27 +32,12 @@ function holliday(peso) {
     return resultado;  
 }
 function superficiecorporal(peso) {
-
 resultado1=((peso*4)+7)/(peso+90)*1500
-resultado2=((peso*4)+7)/(peso+90)*2000
-
-    
-    VOL.innerHTML= resultado1/24 + "cc/hr";
-    VOL.style.display= "block";
-
-    MAN.innerHTML= "m+m/2 ="+ resultado1 / 24 *1.5 +"cc/hr";
-    MAN.style.display="block";
-
-    VOL.innerHTML= resultado2/24 + "cc/hr";
-    VOL.style.display= "block";
-
-    MAN.innerHTML= "m+m/2 ="+ resultado2 / 24 *1.5 +"cc/hr";
-    MAN.style.display="block";
-console.log(resultado1)
-console.log(resultado2)
-
-    return(resultado1,"cc *1500");
-    return(resultado2,"cc *2000");
+resultado2=((peso*4)+7)/(peso+90)*2000 
+    VOL.innerHTML= resultado1 + "cc*1500";
+    VOL.style.display= "block"
+    MAN.innerHTML=  resultado2 +"cc*2000";
+    MAN.style.display="block";   
 }
  
 
